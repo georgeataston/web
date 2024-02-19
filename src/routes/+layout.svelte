9fcfa9654594ea7d808794ac48c01dc372b4cf82
George Hotten <github@ghott.me>
2024-02-19 14:28:14 +0000

    Update footer year

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -10,7 +10,7 @@
 
 <footer class="gweb-footer footer-sticky">
     <div class="content gweb-text-layout footer-text has-text-centered">
-        <p style="font-size: 15px;"><b>© George Hotten 2023</b></p>
+        <p style="font-size: 15px;"><b>© George Hotten 2024</b></p>
         <p style="font-size: 15px;" class="gweb-link-white">
             <a href="https://github.com/georgeataston/web" target="_blank" rel="noopener">source</a> &middot; 
             <a href="https://george.hotten.uk" target="_blank" rel="noopener">personal site</a> &middot; 
